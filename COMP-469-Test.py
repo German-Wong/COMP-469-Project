50e61c6690079b6f6a8a9e116f51a6fcd79c86da
German-Wong <146391452+German-Wong@users.noreply.github.com>
2024-04-19 15:50:13 -0700

    Add files via upload

---
+++ COMP-469-Test.py
@@ -0,0 +1,9 @@
+# Define two variables
+variable1 = 5
+variable2 = 10
+
+# Add the variables
+result = variable1 + variable2
+
+# Print the result
+print("The sum of", variable1, "and", variable2, "is:", result)
