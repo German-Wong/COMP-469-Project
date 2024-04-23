88c3f931fe6716c03391dcf1ffc1716b18e53dd1
German-Wong <146391452+German-Wong@users.noreply.github.com>
2024-04-22 21:22:52 -0700

    Delete COMP-469-Test.py

--- COMP-469-Test.py
+++
@@ -1,9 +0,0 @@
-# Define two variables
-variable1 = 5
-variable2 = 10
-
-# Add the variables
-result = variable1 + variable2
-
-# Print the result
-print("The sum of", variable1, "and", variable2, "is:", result)
